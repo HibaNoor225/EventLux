e27c9895c62348918dce1a089f6a5a3123844d22
Hiba Noor <142714314+HibaNoor225@users.noreply.github.com>
2025-07-16 18:57:56 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 SESSION_TYPE = 'filesystem'
 SESSION_PERMANENT = False
 ADMIN_SECRET= 'adminUser04356'
-SENDGRID_API_KEY = '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'
+SENDGRID_API_KEY = 'Your_Api_key'
 SESSION_FILE_DIR = os.path.abspath('flask-session_custom')  # Custom folder
